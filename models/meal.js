7cd2063c186b1c92e244a8fb445e057604dd6908
ivy20001 <naomijazminsanchezmoreno@gmail.com>
2024-06-06 16:26:27 -0700

    first commit

---
+++ models/meal.js
@@ -0,0 +1,33 @@
+class Meal {
+  constructor(
+    id,
+    categoryIds,
+    title,
+    affordability,
+    complexity,
+    imageUrl,
+    duration,
+    grupos,
+    steps,
+    isGlutenFree,
+    isVegan,
+    isVegetarian,
+    isLactoseFree
+  ) {
+    this.id = id;
+    this.categoryIds = categoryIds;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.grupos = grupos;
+    this.steps = steps;
+    this.duration = duration;
+    this.complexity = complexity;
+    this.affordability = affordability;
+    this.isGlutenFree = isGlutenFree;
+    this.isVegan = isVegan;
+    this.isVegetarian = isVegetarian;
+    this.isLactoseFree = isLactoseFree;
+  }
+}
+
+export default Meal;
